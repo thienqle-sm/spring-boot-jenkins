583af5efb64c64314724581a86173ba813f7ad28
thienqle-sm <68304143+thienqle-sm@users.noreply.github.com>
2023-04-15 00:15:54 -0400

    Update GreetingController.java

--- src/main/java/springboot/mvc/demo/controller/GreetingController.java
+++ src/main/java/springboot/mvc/demo/controller/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	//http://localhost:8080/greeting?name=Test
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String home() {
 		
 		return "index";
